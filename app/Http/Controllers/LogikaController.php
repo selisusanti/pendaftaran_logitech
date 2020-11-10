b609a1d468cca38382f1404f44208f901ccfb934
selisusanti <seli.susanti@indosystem.com>
2020-11-10 11:19:45 +0700

    logika tes

--- app/Http/Controllers/LogikaController.php
+++ app/Http/Controllers/LogikaController.php
@@ -31,6 +31,72 @@ class LogikaController extends Controller
 
     }
 
-    //
+    //bilangan array
+    public function bilanganArray(Request $request)
+    {
+        $jml    = count($request->nilai);
+        $nilai  = $request->nilai;
+
+        for($i=0; $i<$jml-1; $i++)
+        {
+            $j=$i+1;
+            if($nilai[$i]>=$nilai[$j])
+            {
+                $j=$nilai[$j]; 
+            }
+            else
+            {
+                $temp=$nilai[$j];
+                if($nilai[$j]>=$temp)
+                {
+                    $maksimal=$nilai[$j];
+                }
+                else
+                {
+                    $maksimal=$temp;
+                }
+            }
+        }
+
+        return "nilai maksimal : " . $maksimal;
+    }
+
+    //bilangan array
+    public function loopNilai(Request $request)
+    {
+        for($i=1;$i<=$request->nilai;$i++)
+        {
+            for($k=1;$k<=$i;$k++){
+                print $k."<br>";
+            }
+        }
+    }
+
+    //bubble sorting 
+    public function sortingValue(Request $request)
+    {
+        $temp;
+        $array  = $request->nilai;
+        $n      = count($request->nilai);
+
+        for($i=0; $i<$n; $i++) 
+        {
+            for($j=0; $j<$n-$i-1; $j++)
+            {
+                if($array[$j]>$array[$j+1])
+                {
+                    $temp = $array[$j];
+                    $array[$j] = $array[$j+1];
+                    $array[$j+1] = $temp;
+                }
+            }
+        }
+
+        for ($i = 0; $i < $n; $i++) 
+        {
+            echo $array[$i]." "; 
+        } 
+
+    }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::get('bilanganPrima', 'LogikaController@bilanganPrima');
+Route::post('bilanganArray', 'LogikaController@bilanganArray');
+Route::get('loopNilai', 'LogikaController@loopNilai');
+Route::post('sortingValue', 'LogikaController@sortingValue');
